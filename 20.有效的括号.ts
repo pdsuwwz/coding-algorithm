95233fddf1e3d9239c622c87454f439095999eeb
pdsuwwz <pdsu.wwz@foxmail.com>
2022-10-12 18:01:17 +0800

    feat: update comments for [20] 有效的括号

--- 20.有效的括号.ts
+++ 20.有效的括号.ts
@@ -17,16 +17,20 @@ function isValid(str: string): boolean {
   const stack: Array<string> = []
   for (let i = 0; i < str.length; i++) {
     const chars = str[i]
+    // 每当匹配到左括号时，入栈，等待下一次循环能匹配到右括号
     if (['(', '{', '['].includes(chars)) {
       stack.push(chars)
       continue
     }
+    // 若匹配到左右括号，则弹出左括号；否则匹配不到即表示括号不合法
     if (stack[stack.length - 1] === leftMapping[chars]) {
       stack.pop()
     } else {
       return false
     }
   }
+  // 当遇到出现连续左括号的用例时，如 (( {{{ [[[ ，上述循环不会校验
+  // 则需要在最后判断 stack 是否为空，如果不为空则表示没有匹配完全，即不合法
 
   return stack.length === 0
 };
